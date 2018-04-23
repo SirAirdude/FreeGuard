037dbe0590207c4319e1139414671c98f0f7cbbc
SirAirdude <37344835+SirAirdude@users.noreply.github.com>
2018-04-22 22:53:54 -0400

    Add files via upload
    This was created with a temp/alternate database, will need to be changed to primary database

---
+++ FreeGuard/README.md
@@ -0,0 +1,9 @@
+FreeGuard
+-------------------
+
+A group project for 412, involving social security.
+The group members are Matt Stillwell, Lucus ---, Kennan Starstrom, and Kyle ---. 
+
+The URL is: 
+412project.ddns.net  or
+freeguard.ddns.net
---
+++ FreeGuard/src/freeguard/Runner.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freeguard;
+
+/**
+ *
+ * @author Keenan
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //NewUser u = new NewUser();
+        //u.main(null);
+        
+        Login l = new Login();
+        l.main(null);
+        
+
+    }
+    
+}
